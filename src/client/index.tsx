394bdfa70bed306acadcd30e94111fce183d111b
Joshua Harms <joshua@nozzlegear.com>
2018-11-05 18:22:09 -0600

    Parse ErrorResponse objects from server

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -3,7 +3,8 @@ import { render } from "react-dom";
 import { Streamer } from "./streamer";
 import { FAKE_DATA, offlineThumbnail } from "../shared/fake-data";
 import { TwitchUser, GetUserResponse } from "twitch";
-import { StreamerData } from "app";
+import { StreamerData, ErrorResponse } from "app";
+import { Option } from "@nozzlegear/railway";
 
 // import stylus file
 require("./index.styl");
@@ -13,27 +14,42 @@ export interface Props extends React.Props<any> {}
 export interface State {
     loading: boolean;
     streamers: StreamerData[];
+    error: Option<string>;
 }
 
 export class App extends React.Component<Props, State> {
     state: State = {
         loading: true,
-        streamers: []
+        streamers: [],
+        error: Option.ofNone()
     };
+
+    isErrorResponse(r: any): r is ErrorResponse {
+        return r.ok === false && typeof r.status === "number";
+    }
 
     async componentDidMount() {
         const result = await fetch("http://localhost:3000/oauth/tde-streamer-widget");
-        const body: GetUserResponse = await result.json();
+        const body: GetUserResponse | ErrorResponse = await result.json();
 
-        this.setState({
-            loading: false,
-            streamers: body.data.map<StreamerData>(u => ({
-                live: false,
-                offlineThumbnail: u.offline_image_url || offlineThumbnail,
-                service: "twitch",
-                username: u.login
-            }))
-        });
+        if (this.isErrorResponse(body)) {
+            console.error("Failed to get streamer widget data", body);
+
+            this.setState({
+                loading: false,
+                error: Option.ofSome(body.message)
+            });
+        } else {
+            this.setState({
+                loading: false,
+                streamers: body.data.map<StreamerData>(u => ({
+                    live: false,
+                    offlineThumbnail: u.offline_image_url || offlineThumbnail,
+                    service: "twitch",
+                    username: u.login
+                }))
+            });
+        }
     }
 
     render() {
@@ -53,6 +69,7 @@ export class App extends React.Component<Props, State> {
                         <Streamer streamer={streamer} />
                     </div>
                 ))}
+                {this.state.error.map(e => <p style={{ color: "red", fontWeight: "bold" }}>{e}</p>).defaultValue(<></>)}
             </div>
         );
     }
